34c0e687f591168ec9c4df9cdd38a0d1f6b60624
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-09-01 05:46:17 +0700

    init project

---
+++ src/main/java/com/bcorpse/circuitebreaker/CircuiteBreakerApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.circuitebreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuiteBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuiteBreakerApplication.class, args);
+	}
+}
